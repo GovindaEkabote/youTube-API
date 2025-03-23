70154ec12f13cd9ac49886aa2e1e75bcfd741451
GovindaEkbote <govindaekbote7@gmail.com>
2025-03-23 11:26:09 +0530

    get all comment api done

--- Controllers/comment.controller.js
+++ Controllers/comment.controller.js
@@ -20,3 +20,16 @@ exports.createComment = tryCatchError(async (req, res, next) => {
   await newComment.save();
   res.status(200).json({message:"Comment added successfully",newComment})
 });
+
+
+exports.getAllComments = tryCatchError(async(req,res,next) =>{
+    const videoId = req.params.videoId;
+    const comments = await Comment.find({videoId}).populate('userId','username');
+    res.status(200).json({
+        success:true,
+        comments : comments.map((comment) =>({
+            username:comment.userId.username,
+            content:comment.content
+        }))
+    })
+})
--- Routes/comment.routes.js
+++ Routes/comment.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { IsAuthenticatedUser } = require("../Middleware/Auth");
-const { createComment } = require("../Controllers/comment.controller");
+const { createComment, getAllComments } = require("../Controllers/comment.controller");
 const router = express.Router();
 
 
 router.route('/comment').post(IsAuthenticatedUser,createComment)
+router.route('/comment/:videoId').get(IsAuthenticatedUser,getAllComments)
 
 module.exports = router;
