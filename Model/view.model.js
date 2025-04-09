310a144a3ab00f3ced61f91f725d5e0f46cedd9c
GovindaEkbote <govindaekbote7@gmail.com>
2025-04-09 08:54:30 +0530

    Create view Model

---
+++ Model/view.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const viewSchema = new mongoose.Schema({
+    userID:{
+        type:mongoose.Schema.ObjectId,
+        ref: 'User',
+    },
+    videoId:{
+        type:mongoose.Schema.ObjectId,
+        ref: 'Video',
+        required: true,
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model("Video", viewSchema);
